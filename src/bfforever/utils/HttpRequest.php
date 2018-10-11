594b47bcafa13b886c362f70c76f888f8e64d05e
Benjamin Robinet <benjamin.robinet@free.fr>
2018-10-11 18:12:58 +0200

    httprequest

--- src/bfforever/utils/HttpRequest.php
+++ src/bfforever/utils/HttpRequest.php
@@ -12,6 +12,12 @@ class HttpRequest extends AbstractHttpRequest
 {
     public function __construct()
     {
-
+        $this->script_name = $_SERVER['SCRIPT_FILENAME']; //Nom du script
+        if(isset($_SERVER['PATH_INFO']))
+            $this->path_info = $_SERVER['PATH_INFO']; //Décalarer le path_info s'il exisite sinon null
+        $this->root = $_SERVER['DOCUMENT_ROOT'];
+        $this->method = $_SERVER['REQUEST_METHOD']; //Methode de la requête
+        $this->get = $_GET;
+        $this->post = $_POST;
     }
 }
